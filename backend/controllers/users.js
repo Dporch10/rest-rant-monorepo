40fbd34989cfbe16cb419fe4d5d797c84c7dd440
DPorch10 <aintshit@DPorch10s-MacBook-Pro.local>
2024-04-27 15:25:26 -0400

    update

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,15 +1,20 @@
 const router = require('express').Router()
 const db = require("../models")
+const bcrypt = require('bcrypt')
 
 const { User } = db
 
-router.post('/', async (req, res) => {
+router.post("/", async (req, res) => {
+    let { password, ...rest } = req.body;
+    const passwordDigest = await bcrypt.hash(password, 10)
     const user = await User.create(req.body)
     res.json(user)
 })
 
+    
 
-router.get('/', async (req, res) => {
+
+router.get("/", async (req, res) => {
     const users = await User.findAll()
     res.json(users)
 })
